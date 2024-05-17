c681fb0e70bcb2eb31037f0075a2ef26f0062da3
Ankit Kumar <coderankitkumar@gmail.com>
2024-05-17 21:55:14 +0530

    call back implemented

--- src/main/java/learn/kafka/service/MessageProducer.java
+++ src/main/java/learn/kafka/service/MessageProducer.java
@@ -1,13 +1,15 @@
-package com.example.kafka.service;
+package learn.kafka.service;
 
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 @Slf4j
 @Service
@@ -26,10 +28,37 @@ public class MessageProducer {
 		
 		Producer<String, String> producer = new KafkaProducer<>(props);
 		ProducerRecord<String, String> record = new ProducerRecord<>(topicName, key, message);
-		producer.send(record);
-		producer.close();
+		try {
+			
+			// Asynchronous call 
+			producer.send(record, new MyProducerCallBack());
+			
+			// Synchronous call 
+//			RecordMetadata metaData = producer.send(record).get();
+//			System.out.println("Message is sent to partition no: " + metaData.partition()
+//			+ " and offset :" + metaData.offset());
+			System.out.println("Synchronous producer completed.");
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("Synchronous producer failed.");
+		}finally {
+			producer.close();
+		}
 		System.out.print("Producer has sent the message");
 	}
 	
+	class MyProducerCallBack implements Callback{
+
+		@Override
+		public void onCompletion(RecordMetadata metadata, Exception exception) {
+			if(exception!= null) {
+				System.out.print("Asynchronous Produer failed with an exception");
+			}else {
+				System.out.print("Asynchronous Produer call success:");
+			}
+			
+		}}
+	
 
 }
+
