8a3c70d0265c1d965739e62ad1449df13525abc5
Ankit Kumar <coderankitkumar@gmail.com>
2024-05-19 13:05:36 +0530

    Revert "commit Kafka consumer changes"
    This reverts commit f9209d300eea95a1cbb66e56fbb24904ee65f19d.

--- src/main/java/learn/kafka/service/MessageConsumer.java
+++ src/main/java/learn/kafka/service/MessageConsumer.java
@@ -1,14 +1,8 @@
 package learn.kafka.service;
 
-import java.util.Properties;
-
-//import org.apache.kafka.clients.p
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class MessageConsumer {
-	
-	String topicName = "test";
-	Properties prop = new Properties();
 	
 }
